8160d495be2533d20d39e3efb72835672af3c4be
olivierpaes <olivier@paesbrugghe.be>
2016-11-28 22:01:52 +0100

    Added lesson 4 (routing)

---
+++ RabbitMQ-Playground/Routing_Consumer/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Routing_Consumer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare("direct_logs", "direct");
+
+                    var queueName = channel.QueueDeclare().QueueName;
+
+                    if (args.Length < 1)
+                    {
+                        Console.Error.WriteLine("Usage: {0} [info] [warning] [error]",
+                                                Environment.GetCommandLineArgs()[0]);
+                        Console.WriteLine(" Press [enter] to exit.");
+                        Console.ReadLine();
+                        Environment.ExitCode = 1;
+                        return;
+                    }
+
+                    foreach (var severity in args)
+                    {
+                        channel.QueueBind(queue: queueName,
+                                          exchange: "direct_logs",
+                                          routingKey: severity);
+                    }
+
+                    Console.WriteLine(" [*] Waiting for logs.");
+
+                    var consumer = new EventingBasicConsumer(channel);
+                    consumer.Received += (model, ea) =>
+                    {
+                        var body = ea.Body;
+                        var message = Encoding.UTF8.GetString(body);
+                        Console.WriteLine($" [x]  {message}");
+                    };
+                    channel.BasicConsume(queue: queueName,
+                        noAck: true,
+                        consumer: consumer);
+
+                    Console.WriteLine(" Press [enter] to exit.");
+                    Console.ReadLine();
+                }
+            }
+        }
+    }
+}
---
+++ RabbitMQ-Playground/Routing_Producer/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using RabbitMQ.Client;
+
+namespace Routing_Producer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            {
+                using (var channel = connection.CreateModel())
+                {
+                    channel.ExchangeDeclare(exchange: "direct_logs", type: "direct");
+                    var severity = (args.Length > 0) ? args[0] : "info";
+                    var message = GetMessage(args);
+                    var body = Encoding.UTF8.GetBytes(message);
+
+                    channel.BasicPublish(exchange: "direct_logs",
+                        routingKey: severity,
+                        body: body);
+
+                    Console.WriteLine($" [x] Sent {message}");
+                }
+            }
+
+        }
+
+        private static string GetMessage(string[] args)
+        {
+            return ((args.Length > 0) ? string.Join(" ", args) : "info: Hello World!");
+        }
+    }
+}
